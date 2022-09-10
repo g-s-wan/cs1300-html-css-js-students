54bfb143cbb38da6bc3676ecdf2fec3434de4e24
Grace Wan <grace_wan@brown.edu>
2022-09-10 17:58:22 -0400

    Done with studio!

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 // Make a GET request to the fruityvice api to retrieve some fruit data
 const apiRequest = async () => {
   /**
-   * To access information in this API, we need to send our requests through a proxy due to CORS restrictions. 
-   * We'll install a proxy to get around this. Learn more about CORS here https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS. 
-   * 
+   * To access information in this API, we need to send our requests through a proxy due to CORS restrictions.
+   * We'll install a proxy to get around this. Learn more about CORS here https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS.
+   *
    * Step 1: In your terminal and run `npm install -g local-cors-proxy` (if you run into an access error, try `sudo npm install -g local-cors-proxy`)
    * Step 2: Once installation is finished, run `lcp --proxyUrl https://www.fruityvice.com`
-   * Step 3: If you see "Proxy Active", you're all set up! 
-   * 
+   * Step 3: If you see "Proxy Active", you're all set up!
+   *
    * Note the port number (eg. PORT: 8010) and fill it in below
    */
 
-  // TODO fill in your own port number 
-  const PORT_NUMBER = "";
+  // TODO fill in your own port number
+  const PORT_NUMBER = 8010;
 
   const baseUrl = `http://localhost:${PORT_NUMBER}/proxy/api/`
 
@@ -21,7 +21,7 @@ const apiRequest = async () => {
 
   // Making a fetch request to an API endpoint
   // Note: a fetch request is an asynchronous operation, and `await` tells the program to wait until the request has been completed before continuing
-  const response = await fetch(baseUrl + endpoint, { 
+  const response = await fetch(baseUrl + endpoint, {
     method: "GET",
     headers: {
       'Content-Type': 'application/json',
@@ -29,7 +29,7 @@ const apiRequest = async () => {
     }
   });
 
-  // console.log(response);
+  console.log(response);
 
   // Return the response in JSON format
   return response.json();
@@ -40,15 +40,25 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
-  // For example, find "name of all fruits whose sugar > 15", 
+  // For example, find "name of all fruits whose sugar > 15",
+  const newArray = fruitsArray.filter(fruit => {
+    return fruit.name.includes('y')
+  })
 
-  // TODO: Create a new HTML element to display your data 
+  console.log(newArray)
+
+  // TODO: Create a new HTML element to display your data
+  const newEle = document.createElement('div');
+  newEle.innerHTML = newArray.map(fruit => {
+    return fruit.name
+  })
 
   // TODO: Append your new element to the page
-
+  const existingEle = document.getElementById('cs1300-gallery');
+  existingEle.append(newEle)
 }
 
 // SAMPLE CODE of how to create and append a new HTML element to the page
